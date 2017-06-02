b703945a84be478e97f2c6402a9dffcf67a9a358
swleemans <simon.leemans@gmail.com>
2017-06-02 14:45:35 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,39 @@
+//
+//  main.cpp
+//  Helo
+//
+//
+
+#include <iostream>
+
+#include "helo.h"
+std::string Hungry(std::string);
+
+int main() {
+    using namespace std;
+    cout << "Hi Tara";
+    cout << endl;
+    cout << "I wrote you a program" << endl;
+    string hun;
+    cout <<"enter y if hungry, n if not";
+    cin >> hun;
+    std::string answer= Hungry(hun);
+    cout << answer;
+    cout <<endl;
+    return 0;
+    
+
+}
+
+std::string Hungry(std::string j)
+{
+    if (j=="y"){
+        return "Let's get food";
+    }
+       else if (j=="n"){
+            return "Ok";
+        }
+       else{
+           return "you typed an incorrect key";
+       }
+        }
